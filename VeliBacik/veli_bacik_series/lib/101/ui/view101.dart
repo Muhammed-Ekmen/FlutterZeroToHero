21d3b5cc40901a5566b336c2e7e01fbd8a0bf7e3
Muhammed-Ekmen <muhammedsemihekmen@hotmail.com>
2022-11-21 15:54:44 +0300

    push

---
+++ VeliBacik/veli_bacik_series/lib/101/ui/view101.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class OneHundredOne extends StatefulWidget {
+  const OneHundredOne({Key? key}) : super(key: key);
+
+  @override
+  State<OneHundredOne> createState() => _OneHundredOneState();
+}
+
+class _OneHundredOneState extends State<OneHundredOne> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("101 ")),
+    );
+  }
+}
+
+
+
+
+/* 
+Custom Theme Usage
+
+
+*/
